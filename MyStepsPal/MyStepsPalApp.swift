e8b534b7d9e262e41a77ebbeb91e0a7303b702e4
Marcus Marshall <marcus.marshall@imbearyhungry.com>
2024-01-23 23:42:05 -0600

    Initial Commit

---
+++ MyStepsPal/MyStepsPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStepsPalApp.swift
+//  MyStepsPal
+//
+//  Created by Marcus Marshall, Jr on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyStepsPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
